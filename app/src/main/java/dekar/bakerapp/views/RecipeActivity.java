f9821ffa6219bd0d6227ff6981b3c2c0aa13a25b
Denis Karakoluichka <karakoluchka@kassir.ru>
2018-08-15 01:26:31 +0300

    detup adaptive layouts

---
+++ app/src/main/java/dekar/bakerapp/views/RecipeActivity.java
@@ -0,0 +1,58 @@
+
+package dekar.bakerapp.views;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+//import dekar.bakerapp.IdlingResource.SimpleIdlingResource;
+import dekar.bakerapp.R;
+import dekar.bakerapp.adapters.RecipeAdapter;
+import dekar.bakerapp.models.Recipe;
+
+import java.util.ArrayList;
+
+
+public class RecipeActivity extends AppCompatActivity implements RecipeAdapter.ListItemClickListener{
+
+    static String ALL_RECIPES="All_Recipes";
+    static String SELECTED_RECIPES="Selected_Recipes";
+    static String SELECTED_STEPS="Selected_Steps";
+    static String SELECTED_INDEX="Selected_Index";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+       setContentView(R.layout.activity_recipe);
+
+       Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+       setSupportActionBar(myToolbar);
+       getSupportActionBar().setHomeButtonEnabled(false);
+       getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+       getSupportActionBar().setTitle(R.string.app_name);
+    }
+
+    @Override
+    public void onListItemClick(Recipe selectedItemIndex) {
+
+        Bundle selectedRecipeBundle = new Bundle();
+        ArrayList<Recipe> selectedRecipe = new ArrayList<>();
+        selectedRecipe.add(selectedItemIndex);
+        selectedRecipeBundle.putParcelableArrayList(SELECTED_RECIPES,selectedRecipe);
+
+        final Intent intent = new Intent(this, RecipeDetailActivity.class);
+        intent.putExtras(selectedRecipeBundle);
+        startActivity(intent);
+
+    }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+       super.onSaveInstanceState(outState);
+    }
+
+
+}
